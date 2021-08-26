6811635500dc2a1566974886cc63230561e6de88
Jie <y1020329@gmail.com>
2021-08-26 22:50:33 +0800

    Update 2021-08-26T14:50:26.736Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,26 +1,25 @@
-$(function() {
+"use strict";
+
+$(function () {
   console.log('Hello Bootstrap5');
 });
-
-$(function() {
+$(function () {
   $("#my-date").datepicker();
 });
-
-
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
+    clickable: true
+  }
 });
-
 var swiper = new Swiper(".mySwiper-freemode", {
   slidesPerView: 3,
   spaceBetween: 30,
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
+    clickable: true
+  }
 });
+//# sourceMappingURL=all.js.map
