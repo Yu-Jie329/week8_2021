f2703f48bf1cbcc65a3f2d11860c0addb505543f
Jie <y1020329@gmail.com>
2021-08-26 22:47:43 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,26 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+
+$(function() {
+  $("#my-date").datepicker();
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+var swiper = new Swiper(".mySwiper-freemode", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
