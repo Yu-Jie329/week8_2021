a49d7d792b43baf241f6c696f33a63d141a10695
Jie <y1020329@gmail.com>
2021-08-27 17:33:31 +0800

    Update 2021-08-27T09:33:27.701Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,16 +6,33 @@ $(function () {
 $(function () {
   $("#my-date").datepicker();
 });
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".mySwiper-pagination-dynamic", {
   spaceBetween: 30,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true
+    // clickable: true,
+    dynamicBullets: true
   }
 });
 var swiper = new Swiper(".mySwiper-freemode", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
+  breakpointsInverse: true,
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    //當寬度大於等於640
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    960: {
+      slidesPerView: 4,
+      spaceBetween: 30
+    }
+  },
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
